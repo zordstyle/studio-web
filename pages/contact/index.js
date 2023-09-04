a8c77bc0f45b2cbc542cfb8ae3671acdbbc74dcf
taeminahn <ahntaemin@naver.com>
2023-09-04 09:46:22 +0900

    google 길찾기 추가

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Index = () => {
   return (
     <div>
-      
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.6110359509637!2d126.99769681998981!3d37.535606716403315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca398da42e6fb%3A0xe7c6b482dd4f613d!2z7Jyg64uI7Ja47YOA7Jq0IO2VnOuCqA!5e0!3m2!1sko!2skr!4v1693788198785!5m2!1sko!2skr"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 };
