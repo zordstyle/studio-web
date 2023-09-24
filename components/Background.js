ec4534d8a743817ba83f8b3f478227e7af8ec33e
taeminahn <ahntaemin@naver.com>
2023-09-24 13:02:45 +0900

    스타일 보완4

--- components/Background.js
+++ components/Background.js
@@ -7,6 +7,7 @@ import background_webm from "@videos/background.webm";
 const Background = ({ loading }) => {
   // const [load, setLoad] = useState(loading);
   const [hasWindow, setHasWindow] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -14,16 +15,20 @@ const Background = ({ loading }) => {
     }
   }, [hasWindow]);
 
+  const loadComplete = () => {
+    setIsLoading(true);
+  }
+
   return (
     <>
       {hasWindow && (
         <video
-          onLoadedData={loading(true)}
+          onLoadedData={() => {loading(true); loadComplete();}}
           autoPlay
           muted
           loop
           playsinline
-          className={styles.backgroundVideo}
+          className={`${styles.backgroundVideo} ${isLoading && styles.show}`}
         >
           <source src={background_webm} type="video/webm" />
           <source src={background_ogv} type="video/ogv" />
