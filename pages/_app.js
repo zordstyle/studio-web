883aa19412be7df996f5f9b06e8827f30425abed
zordstyle <71073033+zordstyle@users.noreply.github.com>
2024-02-13 13:59:04 +0900

    head 재 추가

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import "@styles/globals.css";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import Head from "next/head";
 
 // export default function App({ Component, pageProps }) {
 //   return <Component {...pageProps} />
